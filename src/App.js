5650b083e971912e19fbed78d874039755e8a984
smd9788 <smd09876@gmail.com>
2019-04-10 11:38:17 -0400

    refactored routes into MainRoutes.js. Changed Header text to Dishly
      - MainRoutes has all auth routes. They are used as a component in
        App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react'
-import './App.scss'
-import { Route } from 'react-router-dom'
-
-import AuthenticatedRoute from './auth/components/AuthenticatedRoute'
-import Header from './header/Header'
-import SignUp from './auth/components/SignUp'
-import SignIn from './auth/components/SignIn'
-import SignOut from './auth/components/SignOut'
-import ChangePassword from './auth/components/ChangePassword'
-
 import Alert from 'react-bootstrap/Alert'
+
+import Header from './header/Header'
+import MainRoutes from './MainRoutes'
+
+import './App.scss'
 
 class App extends Component {
   constructor () {
@@ -43,18 +38,7 @@ class App extends Component {
           </Alert>
         ))}
         <main className="container">
-          <Route path='/sign-up' render={() => (
-            <SignUp alert={this.alert} setUser={this.setUser} />
-          )} />
-          <Route path='/sign-in' render={() => (
-            <SignIn alert={this.alert} setUser={this.setUser} />
-          )} />
-          <AuthenticatedRoute user={user} path='/sign-out' render={() => (
-            <SignOut alert={this.alert} clearUser={this.clearUser} user={user} />
-          )} />
-          <AuthenticatedRoute user={user} path='/change-password' render={() => (
-            <ChangePassword alert={this.alert} user={user} />
-          )} />
+          <MainRoutes alert={this.alert}/>
         </main>
       </React.Fragment>
     )
